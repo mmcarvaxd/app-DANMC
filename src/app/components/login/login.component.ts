4b9fe5345107afc9aa280e4f942ac0f715a4e49d
Matheus Carvalho <matheuscarva@hotmail.com>
2020-09-18 22:12:58 -0300

    melhorias no app

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { LoginService } from './../../services/login.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -12,8 +13,9 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginComponent implements OnInit {
   login = ""
   senha = ""
+  isLogging = false;
 
-  constructor(private loginService: LoginService, private router: Router, private authService: AuthService) { }
+  constructor(private loginService: LoginService, private router: Router, private authService: AuthService, public toastController: ToastController) { }
 
   ngOnInit() {
     if(this.authService.getUser()) {
@@ -21,11 +23,21 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  logar() {
+  async logar() {
+    this.isLogging = true;
     this.loginService.login(this.login, this.senha)
       .subscribe(usuario => {
         localStorage.setItem("usuario", JSON.stringify(usuario))
         this.router.navigate(['/home'])
-      })
+        this.isLogging = false;
+    }, async err => {
+        let alert = await this.toastController.create({
+          message: 'Usuário e/ou senha incorretos!',
+          duration: 2000
+        })
+
+        alert.present()
+        this.isLogging = false;
+    })
   }
 }
